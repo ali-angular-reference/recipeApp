55a2eff7825a17b55876cb94390eb661cd8459a5
Ali <aliharriss1995@gmail.com>
2020-09-20 17:06:19 +1200

    latest updates with services

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -2,6 +2,7 @@ export class Recipe {
   public name: string;
   public desc: string;
   public imagePath: string;
+  public
 
   // what gets called to create the object
   constructor(name:string,desc:string,imagePath:string){
