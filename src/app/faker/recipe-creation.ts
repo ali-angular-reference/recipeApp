27f945d9e155f028fdba498d96e6ed34a9141cb2
aliharriss <aliharriss1995@gmail.com>
2020-11-08 10:55:28 +1300

    can now generate n random ingredients for each recipe

--- src/app/faker/recipe-creation.ts
+++ src/app/faker/recipe-creation.ts
@@ -8,25 +8,25 @@ import { Ingredient } from '../shared/ingredient.model';
 function makeNRecipes(numberOfRecipes:number, numberOfIngs:number){
   const faker = new makeFakeDummyData();
   var recipes: Recipe[] = [];
-  for(var i = 0; i < numberOfRecipes; i++){
+  for(var i = 0; i < numberOfRecipes; i++){ // create i recipes
     var ingredients: Ingredient[] = [];
-    // for(var j = 0; j < numberOfIngs; j++){
-      var name = ingName[selectRandomFromList(ingName)];
-      var meas = measure[selectRandomFromList(measure)];
+    for(var j = 0; j < numberOfIngs; j++){ // create j ingredients for every recipe
+      var name = selectRandomFromList(ingName);
+      var meas = selectRandomFromList(measure);
       var quantity = 2
-      ingredients = [new Ingredient(name, meas, quantity)]
-    // }
-    console.log(ingredients)
+      ingredients.push(new Ingredient(name, meas, quantity))
+    }
+    // console.log(ingredients)
     var recipeName = faker.makeFakeDishName();
-    var recipeDesc = faker.makeFakeAdjective() + ' ' + faker.makeFakeNoun() + ' is ' + taste[selectRandomFromList(taste)]
+    var recipeDesc = faker.makeFakeAdjective() + ' ' + faker.makeFakeNoun() + ' is ' + selectRandomFromList(taste)
     recipes.push(new Recipe(recipeName, recipeDesc, dishImageUrls[i], ingredients));
-    console.log(recipes)
+    // console.log(recipes)
   }
   return recipes;
 }
 
 function selectRandomFromList(list:any){
-  return Math.floor(Math.random()*list.length)
+  return list[Math.floor(Math.random()*list.length)]
 }
 
 export {makeNRecipes};
